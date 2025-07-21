00f9efed9e986187b1259705db72279cbe9d0e29
sofidep08 <sx.dpaguirre@udeo.edu.gt>
2025-07-21 11:23:24 -0600

    factorial

--- actividad8.py
+++ actividad8.py
@@ -1,5 +1,15 @@
+def factorial(numero):
+    if numero ==0 or numero==1:
+        return 1
+    else:
+        print(numero, " * " ,numero-1)
+        return numero*factorial(numero-1)
 
-
+def suma_numeros(numero2):
+    if numero2 ==0:
+        return
+    else:
+        return numero + suma_numeros(numero2)
 opcion = 0
 while opcion != 7:
     print("[1] Calcular el factorial")
@@ -15,3 +25,14 @@ while opcion != 7:
     else:
         match opcion:
             case 1:
+                numero = int(input("Ingrese un numero entero positivo: "))
+                if numero < 0:
+                    print("Ingreso un dato incorrecto")
+                else:
+                    print(factorial(numero))
+            case 2:
+                numero = int(input("Ingrese un numero entero positivo: "))
+                if numero < 0:
+                    print("Ingreso un dato incorrecto")
+                else:
+                    print(suma_numeros(numero))
